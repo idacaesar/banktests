e7a69aedf71b897afa4856032547654b7087852f
borg02 <96430119+borg02@users.noreply.github.com>
2022-05-05 21:00:12 +0200

    Added support for xunit

---
+++ Account_Test.cs
@@ -0,0 +1,17 @@
+using Xunit;
+
+namespace banktests;
+
+public class Account_Test
+{
+    // Testatanrop_indata_förväntatresultat
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}

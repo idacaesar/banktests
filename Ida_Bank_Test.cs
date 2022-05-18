28b8efee45edecb08ede73b3ea46f276541dd4e1
Ida Caesar <idakarincaesar@gmail.com>
2022-05-18 19:58:48 +0200

    Nya bank tester

--- Ida_Bank_Test.cs
+++ Ida_Bank_Test.cs
@@ -2,6 +2,7 @@ using Xunit;
 using TestverktygInlämning2;
 using System.Collections.Generic;
 using Xunit.Abstractions;
+using System.IO;
 
 namespace banktests;
 
@@ -19,14 +20,69 @@ public class Ida_Bank_Test
   {
     // Arrange
     var bank = new Bank();
-    bank.Load("/Users/idacaesar/Documents/GitHub/banktests/data.txt");
+
+    var currentDirectory = Directory.GetCurrentDirectory();
+    bank.Load(currentDirectory + "/../../../data.txt");
 
     // Act
     var customers = bank.GetCustomers();
 
     // Assert
-    //System.Console.WriteLine(customers[0].personalNumber);
     Assert.Equal(3, customers.Count);
     Assert.Equal("19911111", customers[0].personalNumber);
+  }
+
+  [Fact]
+  public void addCustomer_returnedTrue()
+  {
+    // Arrange
+    var bank = new Bank();
+
+    var currentDirectory = Directory.GetCurrentDirectory();
+    bank.Load(currentDirectory + "/../../../data.txt");
+
+    // Act
+    var result = bank.AddCustomer("Linda", "0101011904");
+
+    // Assert
+    Assert.Equal(true, result);
+  }
+
+  [Fact]
+  public void addCustomer_customerWasAdded()
+  {
+    // Arrange
+    var bank = new Bank();
+
+    var currentDirectory = Directory.GetCurrentDirectory();
+    bank.Load(currentDirectory + "/../../../data.txt");
+
+    // Act
+    var result = bank.AddCustomer("Linda", "0101011904");
+
+    // Assert
+    var customers = bank.GetCustomers();
+
+    Assert.Equal(4, customers.Count);
+    Assert.Equal("0101011904", customers[3].personalNumber);
+  }
+
+  [Fact]
+  public void xaddCustomer_customerWasAdded()
+  {
+    // Arrange
+    var bank = new Bank();
+
+    var currentDirectory = Directory.GetCurrentDirectory();
+    bank.Load(currentDirectory + "/../../../data.txt");
+
+    // Act
+    //var result = bank.AddCustomer("Linda", "0101011904");
+
+    // Assert
+    //var customers = bank.GetCustomers();
+
+    //Assert.Equal(4, customers.Count);
+    //Assert.Equal("0101011904", customers[3].personalNumber);
   }
 }

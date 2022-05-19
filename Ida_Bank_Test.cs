12bef9fbe776ebb7e6f986d65cbd0c38629ed80f
Ida Caesar <idakarincaesar@gmail.com>
2022-05-19 14:51:25 +0200

    Nya tester

--- Ida_Bank_Test.cs
+++ Ida_Bank_Test.cs
@@ -3,6 +3,7 @@ using TestverktygInlämning2;
 using System.Collections.Generic;
 using Xunit.Abstractions;
 using System.IO;
+using System.Linq;
 
 namespace banktests;
 
@@ -100,4 +101,91 @@ public class Ida_Bank_Test
     // Assert
     Assert.Equal("19860107", result.personalNumber);
   }
+
+  [Fact]
+  public void changeCustomerName_returnedTrue()
+  {
+    // Arrange
+    var bank = new Bank();
+
+    var currentDirectory = Directory.GetCurrentDirectory();
+    bank.Load(currentDirectory + "/../../../data.txt");
+
+    // Act
+    var result = bank.ChangeCustomerName("Benny", "19911111");
+
+    // Assert
+    var customers = bank.GetCustomers();
+    Assert.Equal(true, result);
+    Assert.Contains(customers, customer => customer.personalNumber == "19911111" && customer.firstName == "Benny");
+  }
+
+  [Fact]
+  public void changeCustomerName_missingPersonalNumber_returnsFalse()
+  {
+    // Arrange
+    var bank = new Bank();
+
+    var currentDirectory = Directory.GetCurrentDirectory();
+    bank.Load(currentDirectory + "/../../../data.txt");
+
+    // Act
+    var result = bank.ChangeCustomerName("Benny", "19873015");
+
+    // Assert
+    Assert.Equal(false, result);
+  }
+
+  [Fact]
+  public void removeCustomer_customerRemoved()
+  {
+    // Arrange
+    var bank = new Bank();
+
+    var currentDirectory = Directory.GetCurrentDirectory();
+    bank.Load(currentDirectory + "/../../../data.txt");
+
+    // Act
+    bank.RemoveCustomer("19760314");
+
+    // Assert
+    var customers = bank.GetCustomers();
+    Assert.Equal(2, customers.Count);
+    Assert.DoesNotContain(customers, customer => customer.personalNumber == "19760314");
+  }
+
+  [Fact]
+  public void AddAccount_accountAdded()
+  {
+    // Arrange
+    var bank = new Bank();
+
+    var currentDirectory = Directory.GetCurrentDirectory();
+    bank.Load(currentDirectory + "/../../../data.txt");
+
+    // Act
+    var result = bank.AddAccount("19860107");
+
+    // Assert
+    var customers = bank.GetCustomers();
+
+    Assert.Equal(3, customers.Count);
+  }
+
+  //[Fact]
+  //public void GetAccount_accountData()
+  //{
+  // Arrange
+  //var bank = new Bank();
+
+  // var currentDirectory = Directory.GetCurrentDirectory();
+  //bank.Load(currentDirectory + "/../../../data.txt");
+
+  // Act
+  //var result = bank.GetAccount("1343113", 1005);
+
+  // Assert
+  // Assert.Equal("Manuel", );
+  //}
+
 }

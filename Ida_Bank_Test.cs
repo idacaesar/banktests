9658c652be4cbd6002e567a065f77b45ff9afe7f
Ida Caesar <idakarincaesar@gmail.com>
2022-05-19 21:44:21 +0200

    Dagens tester

--- Ida_Bank_Test.cs
+++ Ida_Bank_Test.cs
@@ -167,25 +167,79 @@ public class Ida_Bank_Test
     var result = bank.AddAccount("19860107");
 
     // Assert
-    var customers = bank.GetCustomers();
+    var customer = bank.GetCustomer("19860107");
 
-    Assert.Equal(3, customers.Count);
+    Assert.Equal(3, customer.customerAccounts.Count);
   }
 
-  //[Fact]
-  //public void GetAccount_accountData()
-  //{
-  // Arrange
-  //var bank = new Bank();
+  [Fact]
+  public void GetAccount_accountData()
+  {
+    // Arrange
+    var bank = new Bank();
 
-  // var currentDirectory = Directory.GetCurrentDirectory();
-  //bank.Load(currentDirectory + "/../../../data.txt");
+    var currentDirectory = Directory.GetCurrentDirectory();
+    bank.Load(currentDirectory + "/../../../data.txt");
 
-  // Act
-  //var result = bank.GetAccount("1343113", 1005);
+    // Act
+    Account result = bank.GetAccount("19760314", 1005);
 
-  // Assert
-  // Assert.Equal("Manuel", );
-  //}
+    // Assert
+    Assert.Equal("debit", result.accountType);
+    Assert.Equal(1005, result.accountNumber);
+    Assert.Equal(200, result.balance);
+  }
+
+  [Fact]
+  public void GetAccountInfo_returnInfo()
+  {
+    // Arrange
+    var bank = new Bank();
+
+    var currentDirectory = Directory.GetCurrentDirectory();
+    bank.Load(currentDirectory + "/../../../data.txt");
+
+    // Act
+    var result = bank.GetAccountInfo("19860107", 1003);
+
+    // Assert
+    // Otydlig specifikation i kod, vi antar att metoden ska retunera en string som inte är null eller tom
+    Assert.Equal(false, string.IsNullOrEmpty(result));
+  }
+
+
+  [Fact]
+  public void Deposit_balanceIncreased()
+  {
+    // Arrange
+    var bank = new Bank();
+
+    var currentDirectory = Directory.GetCurrentDirectory();
+    bank.Load(currentDirectory + "/../../../data.txt");
+
+    // Act
+    var result = bank.Deposit("19911111", 1001, 5000);
+
+    // Assert
+    var account = bank.GetAccount("19911111", 1001);
+    Assert.Equal(10000, account.balance);
+  }
+
+  [Fact]
+  public void Withdraw_balanceReduction()
+  {
+    // Arrange
+    var bank = new Bank();
+
+    var currentDirectory = Directory.GetCurrentDirectory();
+    bank.Load(currentDirectory + "/../../../data.txt");
+
+    // Act
+    var result = bank.Withdraw("19911111", 1001, 1000);
+
+    // Assert
+    var account = bank.GetAccount("19911111", 1001);
+    Assert.Equal(4000, account.balance);
+  }
 
 }
